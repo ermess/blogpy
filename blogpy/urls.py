4c1b42543345a1a5f7b67725322663b2613e8ac2
sina <74sinahosseini@gmail.com>
2020-08-12 19:16:01 +0430

    Add index.html to blog app

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^', include('blog.urls'))
 ]
