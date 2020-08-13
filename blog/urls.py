b3a05622a94aa1b22221658fdc38c0501b8af4e1
sina <74sinahosseini@gmail.com>
2020-08-13 20:04:35 +0430

    Install Django Rest Framework and Create All Article Api

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexPage.as_view(), name='index'),
     url(r'^contact/$', views.ContactPage.as_view(), name='contact'),
+
+    url(r'^article/all$', views.AllArticleApiView.as_view(), name='all_articles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class IndexPage(TemplateView):
@@ -41,3 +44,27 @@ class IndexPage(TemplateView):
 
 class ContactPage(TemplateView):
     template_name = 'contact.html'
+
+
+class AllArticleApiView(APIView):
+
+    def get(self, request, format=None):
+        try:
+            all_articles = Article.objects.all().order_by('-created_at')[:10]
+            data = []
+
+            for article in all_articles:
+                data.append({
+                    'title': article.title,
+                    'content': article.content,
+                    'cover': article.cover.url if article.cover.url else None,
+                    'created_at': article.created_at,
+                    'category': article.category.title,
+                    'author': article.author.user.first_name + ' ' + article.author.user.last_name,
+                    'promote': article.promote,
+                })
+            return Response({'data': data}, status=status.HTTP_200_OK)
+
+        except:
+            return Response({'status': "Internal Server Error, We'll Check It Later"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
